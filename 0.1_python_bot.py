7afb3ae620732eb52dfe3ab1dc2152155d22410a
crea <59893931+creativityfails@users.noreply.github.com>
2020-01-14 15:53:51 -0600

    add files

---
+++ 0.1_python_bot.py
@@ -0,0 +1,35 @@
+import discord
+import htmlScrape
+import tanklookup as tl
+import botkey
+
+client = discord.Client()
+wot_regions = {'!na', '!eu', '!ru', '!sea'}
+wot_channels = {'hidden-channel', 'wot-uncensored', 'wot'}
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    command = message.content.split(' ', 1)
+
+    if command[0].lower() in wot_regions and message.channel.name in wot_channels:
+        command = message.content.split(' ', 3)
+        if len(command) == 3:
+            answer = htmlScrape.wotlabs_scrape(command[0][1:].lower(), command[1], command[2].lower())
+        else:
+            answer = htmlScrape.wotlabs_scrape(command[0][1:].lower(), command[1])
+        await message.channel.send(answer)
+
+    if command[0].lower() == '!tank':
+        answer = tl.tanklookup(command[1])
+        await message.channel.send(answer)
+
+client.run(botkey.key)
---
+++ htmlScrape.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def wotlabs_scrape(server, name, mod=None):
+    url = 'https://wotlabs.net/' + server + '/player/' + name
+    html = requests.get(url)
+
+    soup = BeautifulSoup(html.content)
+
+    table = soup.find(id="tankerStats")
+    if table:
+        table = table.find("tbody").find_all("tr")
+        battles = table[0].find_all("td")[1].text.strip()
+        if mod == '-r':
+            wr = table[2].find_all("td")[12].text.strip()
+            wn8 = table[13].find_all("td")[6].text.strip()
+            return '*' + name + '* - battles: ' + battles + ', rWR: ' + wr + ', rWN8: ' + wn8 + ' - <' + url + '>'
+        elif mod == "-d":
+            wr = table[2].find_all("td")[4].text.strip()
+            wn8 = table[13].find_all("td")[2].text.strip()
+            if wr == "-":
+                return 'no 24 hour stats for ' + name
+            return '*' + name + '* - battles: ' + battles + ', 24hWR: ' + wr + ', 24hWN8: ' + wn8 + ' - <' + url + '>'
+        else:
+            wr = table[2].find_all("td")[2].text.strip()
+            wn8 = table[13].find_all("td")[1].text.strip()
+            return '*' + name + '* - battles: ' + battles + ', WR: ' + wr + ', WN8: ' + wn8 + ' - <' + url + '>'
+
+    else:
+        return 'Player not found - ' + name
